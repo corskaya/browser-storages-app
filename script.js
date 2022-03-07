0fc12d9f0b4932b0081792d711596821c7729b99
corskaya <cagriorskaya@hotmail.com>
2022-03-07 14:14:47 +0300

    adding data to cookies

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ const key = document.querySelector('#key')
 const value = document.querySelector('#value')
 const storages = document.querySelector('#storages')
 const btnAdd = document.querySelector('#btn-add')
+const span = document.querySelector('#expire-input')
 
 storages.value = 'local-storage'
 
-const resetInput = () => {
-  key.value = ''
-  value.value = ''
-}
+storages.addEventListener('change', () => {
+  renderDefaults()
+
+  if (storages.value === 'cookie-storage') {
+    renderCookieElements()
+  }
+})
 
 btnAdd.addEventListener('click', () => {
   const storagesValue = storages.value
@@ -22,8 +26,37 @@ btnAdd.addEventListener('click', () => {
   } else if (storagesValue === 'session-storage') {
     sessionStorage.setItem(keyValue, valueValue)
   } else if (storagesValue === 'cookie-storage') {
-
+    addCookie()
   }
 
   resetInput()
 })
+
+const resetInput = () => {
+  key.value = ''
+  value.value = ''
+}
+
+const renderDefaults = () => {
+  span.innerHTML = ''
+}
+
+const renderCookieElements = () => {
+  const labelForExpiration = document.createElement('label')
+  labelForExpiration.textContent = 'Expiration date: '
+  labelForExpiration.setAttribute('for', 'expiration')
+  span.appendChild(labelForExpiration)
+
+  const expiration = document.createElement('input')
+  expiration.setAttribute('type', 'date')
+  expiration.setAttribute('id', 'expiration')
+  span.appendChild(expiration)
+}
+
+const addCookie = () => {
+  const expirationDate = document.querySelector('#expiration').value
+  const expirationDateArray = expirationDate.split('-')
+  Cookies.set(key.value, value.value, {
+    expires: new Date(expirationDateArray[0], expirationDateArray[1] - 1, expirationDateArray[2], 3)
+  })
+}
